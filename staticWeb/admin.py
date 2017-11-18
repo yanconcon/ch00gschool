9b15149ae4a537807eb97d20c1177cbc89c04c3a
玩我鸟 <xuakngkoon@foxmail.com>
2017-11-18 19:37:49 +0800

    x11 15

--- staticWeb/admin.py
+++ staticWeb/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Student, Company, Cours
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('username','date_joined','phone')
+    search_fields = ('username',)
+    list_filter = ('date_joined',)
+    ordering = ('-date_joined',)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('username','date_joined','phone')
+    search_fields = ('username',)
+    list_filter = ('date_joined',)
+    ordering = ('-date_joined',)
+
+class CoursAdmin(admin.ModelAdmin):
+    list_display = ('lesson_name','cmpany_associated')
+    search_fields = ('lesson_name',)
+    list_filter = ('release_time',)
+    ordering = ('-release_time',)
+    filter_horizontal = ('student',)
+
+
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Cours,CoursAdmin)
--- staticWeb/models.py
+++ staticWeb/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Student(AbstractUser):
+    studentID = models.IntegerField('学号', blank=True)
+    phone = models.IntegerField('电话号码',blank = False)
+
+    class Meta:
+        db_table = 'Student'
+
+    def __str__(self):
+        return self.username
+
+class Company(AbstractUser):
+    companyID = models.IntegerField('商号',blank=True)
+    phone = models.IntegerField('电话号码',blank=False)
+
+    class Meta:
+        db_table = 'Company'
+
+    def __str__(self):
+        return self.username
+
+class Cours(models.Model):
+    lesson_name = models.CharField(max_length=50)
+    summary = models.TextField()
+    cmpany_associated = models.ManyToManyField(Company)
+    student = models.ManyToManyField(Student)
+    release_time = models.DateField()
+
+
+
+
+

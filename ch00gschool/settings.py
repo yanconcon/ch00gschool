f6df1d65767a2405f50c69dde5239e731c357911
玩我鸟 <xuakngkoon@foxmail.com>
2017-11-20 15:26:00 +0800

    x11 15

--- ch00gschool/settings.py
+++ ch00gschool/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'ch00gschool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'theone',
+        'USER':'postgres',
+        'PASSWORD':'123',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
+
 
 
 # Password validation
